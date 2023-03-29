35c81b5b8b24e8ddde03aca5f1abca2d5e46e9b3
ddeang1t <129127990+ddeang1t@users.noreply.github.com>
2023-03-29 11:14:19 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCdAIwvseyS9RbOnnE21kXBSvsli1fjE9okMnmDecBVQ5KGmVb724Pd8sOuskGEU1llQtW4ELaqadwnmbilIRDG5cS3qb2o6yVEE1DI6ADvadDU3V5hCYnqbVYjT/cYCIFCQhZJvUCEsa8Zbb5nGhYITuHGtwIm/bB4w5XwIu3aMdmeePX5qoCqdL9VvBS9mE9P1jLn9gqEn3wCGW6NvZcj07mq2T3IZOxn5+Q5pOl/19DFDwr6YbOr/R9/9uTdWjgSHnoDIdIttWJfA/YNLwsfYilm707FTj+hI7EpwxY5iLkbvJ4Ecc5xgCqtG6QThfDnWhKh80bWihbKhq8+kXhh root@master.puppet.vm',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCdAIwvseyS9RbOnnE21kXBSvsli1fjE9okMnmDecBVQ5KGmVb724Pd8sOuskGEU1llQtW4ELaqadwnmbilIRDG5cS3qb2o6yVEE1DI6ADvadDU3V5hCYnqbVYjT/cYCIFCQhZJvUCEsa8Zbb5nGhYITuHGtwIm/bB4w5XwIu3aMdmeePX5qoCqdL9VvBS9mE9P1jLn9gqEn3wCGW6NvZcj07mq2T3IZOxn5+Q5pOl/19DFDwr6YbOr/R9/9uTdWjgSHnoDIdIttWJfA/YNLwsfYilm707FTj+hI7EpwxY5iLkbvJ4Ecc5xgCqtG6QThfDnWhKh80bWihbKhq8+kXhh',
   }
 }
