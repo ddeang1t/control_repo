c2c045833cc9a22261cf1248a5e1cb718ede9c13
ddeang1t <129127990+ddeang1t@users.noreply.github.com>
2023-03-29 11:08:39 -0400

    Create ssh_server.pp

---
+++ site/profile/manifests/ssh_server.pp
@@ -0,0 +1,15 @@
+class profile::ssh_server {
+  package {'openssh-server':
+    ensure => present,
+  }
+  service { 'sshd':
+    ensure => 'running',
+    enable => 'true',
+  }
+  ssh_authorized_key { 'root@master.puppet.vm':
+    ensure => present,
+    user   => 'root',
+    type   => 'ssh-rsa',
+    key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCdAIwvseyS9RbOnnE21kXBSvsli1fjE9okMnmDecBVQ5KGmVb724Pd8sOuskGEU1llQtW4ELaqadwnmbilIRDG5cS3qb2o6yVEE1DI6ADvadDU3V5hCYnqbVYjT/cYCIFCQhZJvUCEsa8Zbb5nGhYITuHGtwIm/bB4w5XwIu3aMdmeePX5qoCqdL9VvBS9mE9P1jLn9gqEn3wCGW6NvZcj07mq2T3IZOxn5+Q5pOl/19DFDwr6YbOr/R9/9uTdWjgSHnoDIdIttWJfA/YNLwsfYilm707FTj+hI7EpwxY5iLkbvJ4Ecc5xgCqtG6QThfDnWhKh80bWihbKhq8+kXhh root@master.puppet.vm',
+  }
+}
